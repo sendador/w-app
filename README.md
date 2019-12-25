8e87d112e45b4598f89ad0caaad89023ac5c065d
Kamil <kamil.ulfik@gmail.com>
2019-12-25 19:08:36 +0100

    Register,logout,login basic implement

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![Build Status](https://travis-ci.com/sendador/w-app.svg?branch=master)](https://travis-ci.com/sendador/w-app)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/a9c38df5b8e54a6b93f03826cb6401e5)](https://www.codacy.com/manual/sendador/w-app?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=sendador/w-app&amp;utm_campaign=Badge_Grade)
 # w-app
 Weather app using Django
+
--- wapp/settings.py
+++ wapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'wapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/accounts/dashboard'
+LOGOUT_REDIRECT_URL = '/accounts/dashboard'
